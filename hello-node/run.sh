dbe5d0ebf5022fde15d6622aead65e678cca3871
Pedro J. Molina <pjmolina@gmail.com>
2023-05-22 11:13:17 +0200

    fix volumen mount in windows

--- hello-node/run.sh
+++ hello-node/run.sh
@@ -1,18 +1,18 @@
 docker rm hello1 -f
 
-docker run \
-   -d \
-   --name hello1 \
-   -e DEBUG=yes \
-   -p 8080:80 \
-   hello-node 
-
-VOL1=/app/datos/
-
 # docker run \
 #    -d \
 #    --name hello1 \
 #    -e DEBUG=yes \
 #    -p 8080:80 \
-#    -v 'c/cursos/docker-guadaltel2023/hello-node/datos-es/:${VOL1}:ro' \
 #    hello-node 
+
+VOL1=/app/datos/
+
+docker run \
+   -d \
+   --name hello1 \
+   -e DEBUG=yes \
+   -p 8080:80 \
+   --mount src='/c/cursos/docker-guadaltel2023/hello-node/datos-es/',dst=${VOL1},type=bind \
+   hello-node 
